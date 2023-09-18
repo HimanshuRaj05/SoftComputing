cec03cc58ac534224866b2d2610aec540868a025
Himanshu <charlie05puth@gmail.com>
2023-09-18 23:59:54 +0530

    asdf

--- 3_dfsTraversal.cpp
+++ 3_dfsTraversal.cpp
@@ -1,67 +1,65 @@
-#include<iostream>
-#include<list>
-#include<unordered_set>
+#include <iostream>
+#include <list>
+#include <unordered_set>
 
 using namespace std;
 
-class Graph{
-    public:
-        Graph(int vertices);
-        void addEdge(int v, int w);
-        void dfs(int start);
+class Graph
+{
+public:
+    Graph(int vertices);
+    void addEdge(int v, int w);
+    void dfs(int start);
+    asdfsdsadfasdf
 
-
-    private:
-        int vertices;
-        list<int>* adj;
-        void dfsUtil(int v, unordered_set<int>& visited);
+        private : int vertices;
+    list<int> *adj;
+    void dfsUtil(int v, unordered_set<int> &visited);
 };
 
-Graph :: Graph(int vertices){
-    this->vertices=vertices;
-    adj=new list<int>[vertices];
+Graph ::Graph(int vertices)
+{
+    this->vertices = vertices;
+    adj = new list<int>[vertices];
 }
 
-
-void Graph:: addEdge(int v, int w){
+void Graph::addEdge(int v, int w)
+{
     adj[v].push_back(w);
 }
 
-void Graph:: dfs(int start){
+void Graph::dfs(int start)
+{
     unordered_set<int> visited;
     dfsUtil(start, visited);
 }
 
-
-void Graph::dfsUtil(int v, unordered_set<int>& visited){
+void Graph::dfsUtil(int v, unordered_set<int> &visited)
+{
     visited.insert(v);
-    cout<< v << " ";
+    cout << v << " ";
 
-
-    for(const auto& neighbour: adj[v]){
-        if(visited.find(neighbour)==visited.end()){
-            dfsUtil(neighbour,visited);
+    for (const auto &neighbour : adj[v])
+    {
+        if (visited.find(neighbour) == visited.end())
+        {
+            dfsUtil(neighbour, visited);
         }
     }
-
 }
 
-
-int main(){
+int main()
+{
     Graph g(6);
 
-    g.addEdge(0,1);
-    g.addEdge(0,2);
-    g.addEdge(1,3);
-    g.addEdge(1,4);
-    g.addEdge(2,5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 5);
 
-
-    cout<< "DFS traversal starting from vertex 0:"<< endl;
+    cout << "DFS traversal starting from vertex 0:" << endl;
     g.dfs(0);
-
 
     return 0;
 }
-
-
